3f2379a1b6979ae6c6aee55b6d73876c39606cba
Gustav Burchardt <gustavburchardt@gmail.com>
2020-03-22 22:08:20 +0100

    Update bricks and rename script

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 npm run build
 git add build
-git commit -m "Deploy to gh-pages"
+git commit -m "Upload to gh-pages"
 git remote add pages git@github.com:aufori/aufori.github.io.git
-git subtree push --prefix=build pages master
+git subtree push --prefix=build --force pages master
