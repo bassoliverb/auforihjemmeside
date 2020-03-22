0b2b185b5bd79984186bad26d40fa6aef7eb92b9
Gustav Burchardt <gustavburchardt@gmail.com>
2020-03-22 21:15:25 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 npm run build
-git add -f build
+git add build
 git commit -m "Deploy to gh-pages"
-git remote add pages git@github.com:gustavgb/gustavgb.github.io.git
-git subtree push --prefix build origin gh-pages
+git remote add pages git@github.com:aufori/aufori.github.io.git
+git subtree push --prefix build pages master
