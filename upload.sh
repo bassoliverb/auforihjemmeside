e4aa6813d70fb1e75d0b35ce8a7f37a25bba3936
Gustav Burchardt <gustavburchardt@gmail.com>
2020-03-23 21:40:33 +0100

    Remove build folder

--- upload.sh
+++ upload.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 npm run build
-git add build
+cd build
+git init
+git add .
 git commit -m "Upload to gh-pages"
 git remote add pages git@github.com:aufori/aufori.github.io.git
-git push pages `git subtree split --prefix=build master`:master --force
+git push pages master --force
+# git push pages `git subtree split --prefix=build master`:master --force
