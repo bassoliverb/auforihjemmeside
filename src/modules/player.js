844c263d0b7f65e4359c4bcda86ed4293ba9ec35
Gustav Burchardt <gustavburchardt@gmail.com>
2020-03-30 13:46:31 +0200

    Improve song player system

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,68 +1,6 @@
-const songs = {
-  blaa_maerker: {
-    src: '/audio/blaa_maerker.mp3',
-    title: 'Blå Mærker',
-    album: '/images/blaa-maerker.jpg'
-  },
-  det_tomme_rum: {
-    src: '/audio/det_tomme_rum.mp3',
-    title: 'Det Tomme Rum',
-    album: '/images/overflader-album.jpg'
-  },
-  doed_over_tid: {
-    src: '/audio/doed_over_tid.mp3',
-    title: 'Død Over Tid',
-    album: '/images/overflader-album.jpg'
-  },
-  hjemloes: {
-    src: '/audio/hjemloes.mp3',
-    title: 'Hjemløs',
-    album: '/images/runddans.jpg'
-  },
-  kaffekopper: {
-    src: '/audio/kaffekopper.mp3',
-    title: 'Kaffekopper',
-    album: '/images/runddans.jpg'
-  },
-  luft: {
-    src: '/audio/luft.mp3',
-    title: 'Luft',
-    album: '/images/runddans.jpg'
-  },
-  overflader: {
-    src: '/audio/overflader.mp3',
-    title: 'Overflader',
-    album: '/images/overflader-album.jpg'
-  },
-  runddans: {
-    src: '/audio/runddans.mp3',
-    title: 'Runddans',
-    album: '/images/runddans.jpg'
-  },
-  roed: {
-    src: '/audio/roed.mp3',
-    title: 'Rød',
-    album: '/images/overflader-album.jpg'
-  },
-  stjernetraade: {
-    src: '/audio/stjernetraade.mp3',
-    title: 'Stjernetråde',
-    album: '/images/stjernetraade.jpg'
-  },
-  traette_voksne: {
-    src: '/audio/traette_voksne.mp3',
-    title: 'Trætte Voksne',
-    album: '/images/runddans.jpg'
-  },
-  aabne_favne: {
-    src: '/audio/aabne_favne.mp3',
-    title: 'Åbne Favne',
-    album: '/images/runddans.jpg'
-  }
-}
-
 let currentAudio = null
 let currentSong = null
+let currentSongEl = null
 
 function padNum (n) {
   return n < 10 ? '0' + n : n
@@ -79,33 +17,36 @@ function formatTime (time) {
   return minutes + ':' + padNum(seconds)
 }
 
-window.playSong = function playSong (id) {
-  if (currentSong === id) {
+window.playSong = function playSong (buttonEl, src) {
+  if (currentSong === src) {
     return window.toggleSong()
   }
 
-  if (currentAudio) {
+  if (currentAudio && currentSongEl) {
     currentAudio.pause()
-    document.getElementById('song-play-' + currentSong).src = '/images/play-icon.svg'
+    currentSongEl.querySelector('.play-song-btn img').src = '/images/play-icon.svg'
   }
 
-  currentSong = id
-  const song = songs[id]
+  currentSongEl = buttonEl.closest('.column')
+  currentSong = src
 
-  const audio = new window.Audio(song.src)
+  const albumArt = currentSongEl.querySelector('.img-track > img').src
+  const songTitle = currentSongEl.querySelector('h1').innerText
+
+  const audio = new window.Audio(src)
   currentAudio = audio
   audio.addEventListener('canplay', function () {
     audio.play()
     document.getElementById('player').classList.remove('hidden')
-    document.getElementById('song-title').innerText = song.title
+    document.getElementById('song-title').innerText = songTitle
     document.getElementById('song-time').innerText = '0:00 / ' + formatTime(audio.duration)
     document.getElementById('button-play-icon').classList.add('hidden')
     document.getElementById('button-pause-icon').classList.remove('hidden')
-    document.getElementById('song-play-' + id).src = '/images/pause-icon.svg'
+    currentSongEl.querySelector('.play-song-btn img').src = '/images/pause-icon.svg'
     document.getElementById('button-stop').classList.add('hidden')
     const songAlbumEl = document.getElementById('song-album')
-    if (song.album) {
-      songAlbumEl.src = song.album
+    if (albumArt) {
+      songAlbumEl.src = albumArt
       songAlbumEl.classList.remove('hidden')
     } else {
       songAlbumEl.src = ''
@@ -123,20 +64,20 @@ window.toggleSong = function toggleSong () {
     currentAudio.play()
     document.getElementById('button-play-icon').classList.add('hidden')
     document.getElementById('button-pause-icon').classList.remove('hidden')
-    document.getElementById('song-play-' + currentSong).src = '/images/pause-icon.svg'
+    currentSongEl.querySelector('.play-song-btn img').src = '/images/pause-icon.svg'
     document.getElementById('button-stop').classList.add('hidden')
   } else {
     currentAudio.pause()
     document.getElementById('button-play-icon').classList.remove('hidden')
     document.getElementById('button-pause-icon').classList.add('hidden')
-    document.getElementById('song-play-' + currentSong).src = '/images/play-icon.svg'
+    currentSongEl.querySelector('.play-song-btn img').src = '/images/play-icon.svg'
     document.getElementById('button-stop').classList.remove('hidden')
   }
 }
 
 window.stopSong = function stopSong () {
   if (currentSong && currentAudio) {
-    document.getElementById('song-play-' + currentSong).src = '/images/play-icon.svg'
+    currentSongEl.querySelector('.play-song-btn img').src = '/images/play-icon.svg'
     document.getElementById('button-stop').classList.add('hidden')
     document.getElementById('player').classList.add('hidden')
     currentAudio.pause()
