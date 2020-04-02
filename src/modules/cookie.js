455ee5b56d86220dc818dac0c4f159dcfe16f716
Gustav Burchardt <gustavburchardt@gmail.com>
2020-04-02 13:24:55 +0200

    Implement cookie warning

--- src/modules/cookie.js
+++ src/modules/cookie.js
@@ -1,3 +1,13 @@
-window.cookieReadmore = function cookieReadmore (readmoreButton) {
-  readmoreButton.closest('.cookie-warning').classList.add('open')
+window.cookieReadmore = function cookieReadmore () {
+  document.getElementById('cookie-warning').classList.add('open')
+}
+
+window.acceptCookies = function acceptCookies () {
+  window.localStorage.setItem('cookie-consent', 'accepted')
+  document.getElementById('cookie-warning').classList.remove('show')
+}
+
+window.rejectCookies = function acceptCookies () {
+  window.localStorage.setItem('cookie-consent', 'rejected')
+  document.getElementById('cookie-warning').classList.remove('show')
 }
