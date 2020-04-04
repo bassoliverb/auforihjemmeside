1d050d7412edde4d75ca21f9b794c37ac45101f6
bassoliverb <oliver.bernburg@gmail.com>
2020-04-04 23:09:52 +0200

    nothin

--- src/noder/noder.js
+++ src/noder/noder.js
@@ -1,30 +1,62 @@
-const openNodeDivs = document.querySelectorAll('[data-node-target]');
-const closeNodeButtons = document.querySelectorAll('[data-close-button]');
-const overlay = document.getElementById('overlay');
+//const openNodeDivs = document.querySelectorAll('[data-node-target]');
+//const closeNodeButtons = document.querySelectorAll('[data-close-button]');
+//const overlay = document.getElementById('overlay');
 
 
-openNodeDivs.forEach(div => {
-	div.addEventListener("click", () => {
-		const node = document.querySelector(div.dataset.modalTarget);
+//openNodeDivs.forEach(div => {
+//	div.addEventListener("click", () => {
+//		const node = document.querySelector(div.dataset.modalTarget);
+//		openNode(node);
+//	})
+//})
+
+//closeNodeButtons.forEach(div => {
+//	div.addEventListener("click", () => {
+//		const node = button.closest(".popup");
+//		closeNode(node);
+//	})
+//})
+
+//function openNode(node){
+//	if (node == null) return;
+//	node.classList.add("active");
+//	overlay.classList.add("active");
+//}
+
+//function closeNode(node){
+//	if (node == null) return;
+//	node.classList.remove("active");
+//	overlay.classList.remove("active");
+//}
+
+
+var openButtons = document.querySelectorAll("[data-node-target]");
+var closeButtons = document.querySelectorAll("data-close-button");
+var overlay = document.getElementById("overlay");
+
+
+openButtons.forEach(div, function(){
+	div.addEventlistener("click", function(){
+		const node = document.querySelector(div.dataset.nodeTarget);
 		openNode(node);
-	})
-})
+	});
+});
 
-closeNodeButtons.forEach(div => {
-	div.addEventListener("click", () => {
+closeButtons.forEach(button, function(){
+	button.addEventlistener("click", function(){
 		const node = button.closest(".popup");
 		closeNode(node);
-	})
-})
+	});
+});
 
 function openNode(node){
 	if (node == null) return;
 	node.classList.add("active");
-	overlay.classList.add("active");
-}
+	overlay.classlist.add("active");
+};
 
 function closeNode(node){
 	if (node == null) return;
 	node.classList.remove("active");
-	overlay.classList.remove("active");
-}
+	overlay.classlist.remove("active");
+};
