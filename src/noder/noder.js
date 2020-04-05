bf9e280282859b8a06c9e2aff98e4db04d57175a
Gustav Burchardt <gustavburchardt@gmail.com>
2020-04-05 16:16:11 +0200

    Fix noder js

--- src/noder/noder.js
+++ src/noder/noder.js
@@ -1,62 +1,32 @@
-//const openNodeDivs = document.querySelectorAll('[data-node-target]');
-//const closeNodeButtons = document.querySelectorAll('[data-close-button]');
-//const overlay = document.getElementById('overlay');
+var openButtons = document.querySelectorAll('[data-node-target]')
+var closeButtons = document.querySelectorAll('[data-close-button]')
+var overlay = document.getElementById('overlay')
 
+console.log(openButtons)
+console.log(closeButtons)
 
-//openNodeDivs.forEach(div => {
-//	div.addEventListener("click", () => {
-//		const node = document.querySelector(div.dataset.modalTarget);
-//		openNode(node);
-//	})
-//})
+openButtons.forEach(function (div) {
+  div.addEventListener('click', function () {
+    const node = document.querySelector(div.dataset.nodeTarget)
+    openNode(node)
+  })
+})
 
-//closeNodeButtons.forEach(div => {
-//	div.addEventListener("click", () => {
-//		const node = button.closest(".popup");
-//		closeNode(node);
-//	})
-//})
+closeButtons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    const node = button.closest('.popup')
+    closeNode(node)
+  })
+})
 
-//function openNode(node){
-//	if (node == null) return;
-//	node.classList.add("active");
-//	overlay.classList.add("active");
-//}
-
-//function closeNode(node){
-//	if (node == null) return;
-//	node.classList.remove("active");
-//	overlay.classList.remove("active");
-//}
-
-
-var openButtons = document.querySelectorAll("[data-node-target]");
-var closeButtons = document.querySelectorAll("data-close-button");
-var overlay = document.getElementById("overlay");
-
-
-openButtons.forEach(div, function(){
-	div.addEventlistener("click", function(){
-		const node = document.querySelector(div.dataset.nodeTarget);
-		openNode(node);
-	});
-});
-
-closeButtons.forEach(button, function(){
-	button.addEventlistener("click", function(){
-		const node = button.closest(".popup");
-		closeNode(node);
-	});
-});
-
-function openNode(node){
-	if (node == null) return;
-	node.classList.add("active");
-	overlay.classlist.add("active");
+function openNode (node) {
+  if (node == null) return
+  node.classList.add('active')
+  overlay.classList.add('active')
 };
 
-function closeNode(node){
-	if (node == null) return;
-	node.classList.remove("active");
-	overlay.classlist.remove("active");
+function closeNode (node) {
+  if (node == null) return
+  node.classList.remove('active')
+  overlay.classList.remove('active')
 };
