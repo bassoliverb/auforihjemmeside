b332c436bd5eded5532c56a1280d8a1f793b9408
bassoliverb <oliver.bernburg@gmail.com>
2021-02-21 01:16:25 +0100

    update presseside

--- src/presseside/script.js
+++ src/presseside/script.js
@@ -8,6 +8,7 @@ let track_image = document.querySelector('#track_image');
 let present = document.querySelector('#present');
 let total = document.querySelector('#total');
 let release = document.querySelector('#release');
+let curtimetext = document.querySelector('#curtimetext');
 let curtimeText = document.querySelector('#curtimeText');
 let select1 = document.querySelector('#songSelect1')
 let select2 = document.querySelector('#songSelect2')
@@ -27,21 +28,30 @@ let all_song = [
         path: "/audio/blode_universer.mp3",
         img: "/images/blode_universer.png",
         udgivelse: "Single 2021",
-        tid: "5:27"
+        tid: "5:27",
+        underline1: "solid 2px #ffc967",
+        underline2: "none",
+        underline3: "none"
     },
     {
         name: "Åbne Favne",
         path: "/audio/aabne_favne.mp3",
         img: "/images/runddans.jpg",
         udgivelse: "Runddans (album 2018)",
-        tid: "2:25"
+        tid: "2:25",
+        underline1: "none",
+        underline2: "solid 2px #ffc967",
+        underline3: "none"
     },
     {
         name: "Stjernetråde",
         path: "/audio/stjernetraade.mp3",
         img: "/images/overflader-album.jpg",
         udgivelse: "Overflader (album 2020)",
-        tid: "4:11"
+        tid: "4:11",
+        underline1: "none",
+        underline2: "none",
+        underline3: "solid 2px #ffc967"
     }
 
 ];
@@ -52,6 +62,9 @@ function load_track(index_no){
     track_image.src = all_song[index_no].img;
     release.innerHTML = all_song[index_no].udgivelse;
     curtimeText.innerHTML = all_song[index_no].tid;
+    select1.style.borderBottom = all_song[index_no].underline1;
+    select2.style.borderBottom = all_song[index_no].underline2;
+    select3.style.borderBottom = all_song[index_no].underline3;
     track.load();
 
     total.innerHTML = all_song.length;
@@ -129,7 +142,7 @@ function range_slider(){
 
 
 
-
+/* SONG SELECTOR*/
 
 function songselect1(){
     index_no = 0;
@@ -153,6 +166,53 @@ function songselect3(){
 
 
 
+/* BJÆLKE ANIMATION*/
+
+function select1Hover(x){
+    if(index_no !== 0){
+        x.style.borderBottom = "solid 2px rgb(208, 208, 208)"
+    };
+}
+
+function select1DeHover(x){
+    if(index_no === 0){
+        x.style.borderBottom = "solid 2px #ffc967"
+    }else {
+        x.style.borderBottom = "none"
+    };
+}
+
+
+function select2Hover(x){
+    if(index_no !== 1){
+        x.style.borderBottom = "solid 2px rgb(208, 208, 208)"
+    };
+}
+
+function select2DeHover(x){
+    if(index_no === 1){
+        x.style.borderBottom = "solid 2px #ffc967"
+    }else {
+        x.style.borderBottom = "none"
+    };
+}
+
+
+function select3Hover(x){
+    if(index_no !== 2){
+        x.style.borderBottom = "solid 2px rgb(208, 208, 208)"
+    };
+}
+
+function select3DeHover(x){
+    if(index_no === 2){
+        x.style.borderBottom = "solid 2px #ffc967"
+    }else {
+        x.style.borderBottom = "none"
+    };
+}
+
+
 
     //Engelsk tekst//
 
@@ -160,6 +220,8 @@ function songselect3(){
     const btn2 = document.getElementById('language2');
     const dansk = document.getElementById('dansk');
     const engelsk = document.getElementById('engelsk');
+    const dansk1 = document.getElementById('dansk1');
+    const engelsk1 = document.getElementById('engelsk1');
 
 btn1.addEventListener('click', () =>{
     btn1.classList.remove('active');
@@ -168,6 +230,10 @@ btn1.addEventListener('click', () =>{
     dansk.classList.add('inactive')
     engelsk.classList.add('active');
     engelsk.classList.remove('inactive');
+    dansk1.classList.remove('active');
+    dansk1.classList.add('inactive')
+    engelsk1.classList.add('active');
+    engelsk1.classList.remove('inactive');
 })
 
 btn2.addEventListener('click', () =>{
@@ -177,5 +243,9 @@ btn2.addEventListener('click', () =>{
     dansk.classList.remove('inactive')
     engelsk.classList.remove('active');
     engelsk.classList.add('inactive');
+    dansk1.classList.add('active');
+    dansk1.classList.remove('inactive')
+    engelsk1.classList.remove('active');
+    engelsk1.classList.add('inactive');
 })
     
